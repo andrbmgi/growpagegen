02455f44dc25540d5b68af450e3ef38244955535
andrbmgi <andrbmgi@users.noreply.github.com>
2024-08-07 18:45:49 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+git pull
+# Check if pull was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to pull changes from the repository."
+  deactivate
+  exit 1
+fi
+
 # Path to the virtual environment
 VENV_PATH="venv"
 
